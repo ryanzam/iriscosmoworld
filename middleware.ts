b576ddb5a929c9e8fc73b54d4abb0722828ff7ff
ranjam <ryanzam2005@yahoo.com>
2024-05-30 07:24:01 +0300

    added middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,15 @@
-export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+
+    const url = request.nextUrl.pathname;
+    
+    if (url.startsWith("/api")) {
+        NextResponse.next().headers.append("Access-Control-Allow-Origin", "*")
+        NextResponse.next().headers.append("Content-Type", "application/json")
+    }
+}
+
 
 export const config = {
     matcher: [
