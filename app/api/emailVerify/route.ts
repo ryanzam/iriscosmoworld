95985cde728913809343353070dd0ca0c39b3087
ranjam <ryanzam2005@yahoo.com>
2024-06-05 12:54:33 +0300

    fix base url

--- app/api/emailVerify/route.ts
+++ app/api/emailVerify/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 import mongoConnect from "@/lib/mongoConnect"
 import { ObjectId } from "mongodb";
 import User from "@/models/user";
+import { PROD_URL } from "@/utils/constants";
 
 export async function GET(request: NextRequest) {
     await mongoConnect()
@@ -19,7 +20,7 @@ export async function GET(request: NextRequest) {
     if (userFound && !(userFound.emailVerified)) {
         userFound.emailVerified = true
         await User.findOneAndUpdate({ _id: new ObjectId(userFound._id) }, { $set: userFound })
-        return NextResponse.redirect(`${process.env.BASE_URL}/user`)
+        return NextResponse.redirect(`${PROD_URL}/user`)
     }
 
     return NextResponse.error()
