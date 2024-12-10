662a88f655b1cfd8f26a922ee26d35dc294e12e3
ranjam <johndoe@email.com>
2024-12-10 09:04:20 +0545

    fix warnings

--- app/components/Empty.tsx
+++ app/components/Empty.tsx
@@ -24,12 +24,12 @@ const Empty: FC<IEmptyProps> = ({ title, alertClass = "", showResetBtn = false }
         <div className={`alert ` + alertClass}>
             {renderSvg()}
             <div>
-                <h3 className='my-3' className="font-bold">{title}</h3>
+                <h3 className='my-3 font-bold'>{title}</h3>
                 {showResetBtn && <div className="text-xs">Try changing filters</div>}
             </div>
             {showResetBtn && <button className="btn btn-sm" onClick={() => router.push("/")}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                 </svg>
                 Reset
             </button>}
